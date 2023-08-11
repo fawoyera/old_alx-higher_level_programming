55acadabcacce1ddebd03787748f707007bcaa39
fawoyera <femiawoyera@gmail.com>
2023-08-11 07:12:41 -0700

    check if a singly linked list contain a cycle (loop) in it

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,29 +1,23 @@
 #include "lists.h"
 
 /**
- * check_cycle - check if a singly linked list has a cycle in it
+ * check_cycle - check if a singly linked list has a cycle(loop) in it
  * @list: pointer to the linked list
  *
  * Return: 0 if there is no cycle, 1 if there is a cycle
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *temp, *search;
-	int i = 0, j;
+	listint_t *lag, *lead;
 
-	temp = list;
-	while (temp)
+	lag = list;
+	lead = list;
+	while (lag && lead && lead->next)
 	{
-		i++;
-		search = list;
-		for (j = 0; j < i; j++)
-		{
-			if (temp->next == search)
-				return (1);
-			search = search->next;
-		}
-
-		temp = temp->next;
+		lag = lag->next;
+		lead = lead->next->next;
+		if (lag == lead)
+			return (1);
 	}
 
 	return (0);
