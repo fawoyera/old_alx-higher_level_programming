71205d36bdef6c8b5c0353f3aaeb5c35214d79f9
fawoyera <femiawoyera@gmail.com>
2023-09-01 14:47:22 -0700

    create a class Square with private instance attributes (size) and (position) and two public instance methods

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""Module that defines class with private var, public method, g(s)etters"""
+
+
+class Square:
+    """Class that defines a square with optional instance variable & method"""
+    def __init__(self, size=0, position=(0, 0)):
+        """Method that initializes an instance of class Square"""
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        """Method that gets the size of square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Method that sets the size of square"""
+        try:
+            assert type(value) == int
+        except Exception:
+            raise TypeError('size must be an integer')
+        else:
+            try:
+                assert value >= 0
+            except Exception:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = value
+
+    @property
+    def position(self):
+        """Method that gets the position of square"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Method that sets the position of a square"""
+        try:
+            assert type(position[0]) == int and type(position[1]) == int
+        except Exception:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        else:
+            self.__position = value
+
+    def area(self):
+        """Method that returns the current square area"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Method that prints a square using # at a given position"""
+        for i in range(self.__position[1]):
+            print()
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print("{:s}{:s}".format('_' * self.__position[0],
+                      '#' * self.__size))
