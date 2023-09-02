231fe7c049a3ad755af18bd2890cf377595a74c5
fawoyera <femiawoyera@gmail.com>
2023-09-02 14:36:47 -0700

    python class that does exactly the same as given python bytecode

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -5,10 +5,10 @@ import math
 
 class MagicClass:
     """Class that does same as byte code"""
-    def __init__(self, radius):
+    def __init__(self, radius=0):
         """Method that initializes an instance of the class MagicClass"""
         try:
-            assert type(radius) is not int or type(radius) is not float
+            assert type(radius) is int or type(radius) is float
         except Exception:
             raise TypeError('radius must be a number')
         else:
@@ -20,4 +20,4 @@ class MagicClass:
 
     def circumference(self):
         """Method to compute circumference of circle"""
-        return 2 * math.pi * self.__radius ** 2
+        return 2 * math.pi * self.__radius
