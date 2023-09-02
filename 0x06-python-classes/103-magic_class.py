f376c6e29e0efc9a4c2852bc30f37a7477d4a692
fawoyera <femiawoyera@gmail.com>
2023-09-02 14:24:44 -0700

    python class that does exactly the same as given python bytecode

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import math
+"""Module that does exactly the same as given python bytecode"""
+
+
+class MagicClass:
+    """Class that does same as byte code"""
+    def __init__(self, radius):
+        """Method that initializes an instance of the class MagicClass"""
+        try:
+            assert type(radius) is not int or type(radius) is not float
+        except Exception:
+            raise TypeError('radius must be a number')
+        else:
+            self.__radius = radius
+
+    def area(self):
+        """Method to compute area of circle"""
+        return (self.__radius ** 2) * math.pi
+
+    def circumference(self):
+        """Method to compute circumference of circle"""
+        return 2 * math.pi * self.__radius ** 2
