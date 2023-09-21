8fd96ae7f6ffaa332b4d643c9fae408e9fe787a3
fawoyera <femiawoyera@gmail.com>
2023-09-21 21:15:26 +0000

    indent text by printing 2 new lines after ., ? and : with tests

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module to indent text by print 2 new lines after ., ? and :
+Args: text - the tezt string to indent
+Return: None
+"""
+
+
+def text_indentation(text):
+    """
+    Function to indent text by printing 2 new lines after ., ? and :
+    """
+    if not type(text) is str:
+        raise TypeError("text must be a string")
+    for char in range(len(text)):
+        if (text[char - 1] in ['.', '?', ':']) and (text[char] == " "):
+            continue
+        print(text[char], end="")
+        if text[char] in ['.', '?', ':']:
+            print('\n')
