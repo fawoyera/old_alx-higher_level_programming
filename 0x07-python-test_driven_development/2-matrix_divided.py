c0f5240085ebebed5a2681d1b35db3be8d5442d1
fawoyera <femiawoyera@gmail.com>
2023-09-21 16:20:26 +0000

    function that divides all matrix with test file

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -26,6 +26,8 @@ def matrix_divided(matrix, div):
             raise TypeError("Each row of the matrix must have the same size")
     if not type(div) in [int, float]:
         raise TypeError("div must be a number")
+    if div == div + 1:
+        raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
     return list(list(float("{:.2f}".format(element / div))
