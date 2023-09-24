268e76ed6389e1f171cc7fe677958768b63ca3a4
fawoyera <femiawoyera@gmail.com>
2023-09-24 20:04:39 +0000

    multiply two matrices using NumPy with doctest

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -45,6 +45,16 @@ def lazy_matrix_mul(m_a, m_b):
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
     """
+    if (type(m_a) is str) or (type(m_b) is str):
+        raise TypeError("Scalar operands are not allowed, use '*' instead")
+    for item in m_a:
+        for items in item:
+            if type(items) is str:
+                raise TypeError("invalid data type for einsum")
+    for item in m_b:
+        for items in item:
+            if type(items) is str:
+                raise TypeError("invalid data type for einsum")
 
     """ïnitialize result matrix with zeros"""
     result = [[0 for j in range(len(m_b[0]))] for i in range(len(m_a))]
