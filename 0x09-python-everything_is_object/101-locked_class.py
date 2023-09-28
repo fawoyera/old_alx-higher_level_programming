d146386ff7d5f55a4f8e03f8461a9ec8e3ef81af
fawoyera <femiawoyera@gmail.com>
2023-09-28 08:34:07 +0000

    allocates static mememory for class or instance attribute

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -8,17 +8,4 @@ class LockedClass(object):
     """Class that prevents the user from dynamically creating new instance
     attribute, except if the new instance attribute is called first_name
     """
-    def __init__(self):
-        """
-        initialization method for class instance
-        """
-        pass
-
-    def __setattr__(self, key, value):
-        """
-        method to set instance attribute if it is firstname
-        """
-        if key != "first_name":
-            raise AttributeError("'{}' object has no attribute \
-'{}'".format(self.__class__.__name__, key))
-        object.__setattr__(self, key, value)
+    __slots__ = ['first_name']
