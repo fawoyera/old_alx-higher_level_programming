c1985df86a2d2aa946916f541b46dbfde1714f17
fawoyera <femiawoyera@gmail.com>
2023-09-29 07:56:20 +0000

    print list of available attributes and methods of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module to return the list of available attributes & methods of an object"""
+
+
+def lookup(obj):
+    """Function to return list of the attributes and methods of object"""
+    return [keys for keys in obj.__dict__]
