04533d5f6594f466ce226fd8d1d09c37096cac57
fawoyera <femiawoyera@gmail.com>
2023-09-30 06:34:07 +0000

    define a class to validate attributes of a rectangle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,29 +2,13 @@
 """Module that defines a class with two public instance methods"""
 
 
-class BaseGeometry:
-    """Class to validate an integer value"""
-    def __init__(self):
-        """Initialization method"""
-        pass
-
-    def area(self):
-        """Method that does nothing"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Method that validates an integer value"""
-        if not type(value) is int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
     """A subclass of BaseGeometry"""
     def __init__(self, width, height):
         """Ïnitialization method for rectangle attributes width and height"""
-        BaseGeometry.__init__(self)
         if Rectangle.integer_validator(self, "width", width) is None:
             self.__width = width
         if Rectangle.integer_validator(self, "height", height) is None:
